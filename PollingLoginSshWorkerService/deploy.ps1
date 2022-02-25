ff98fa465eaa34c5efa924591f29ba434ef4696e
Cyrus Wong <cywong@vtc.edu.hk>
2022-02-25 09:09:01 +0800

    Update install and uninstall powershell.

--- PollingLoginSshWorkerService/deploy.ps1
+++
@@ -1,4 +0,0 @@
-sc.exe create "PollingLoginSshWorkerService" start=auto binpath="PollingLoginSshWorkerService.exe"
-sc.exe failure "PollingLoginSshWorkerService" reset=0 actions=restart/60000/restart/60000/restart/60000
-sc.exe query "PollingLoginSshWorkerService"
-sc.exe qfailure "PollingLoginSshWorkerService"
